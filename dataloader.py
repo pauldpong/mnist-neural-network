6c45a4460862b0088a21cf68b592b0abb68fa791
Soraphol (Paul) Damrongpiriyapong <pauldamrong@gmail.com>
2018-05-02 21:48:10 -0400

    Added labels

--- dataloader.py
+++ dataloader.py
@@ -4,27 +4,46 @@ import numpy as np
 def load_data():
     print("Unzipping...")
     training_images = gzip.open("./MNIST/trainimages.gz", 'rb')
+    training_labels = gzip.open("./MNIST/trainlabels.gz", 'rb')
 
     try:
         print("Reading...")
         training_images.read(4)
+        training_labels.read(8)
 
-        num__images = int.from_bytes(training_images.read(4), byteorder="big")
+        num_images = int.from_bytes(training_images.read(4), byteorder="big")
         num_rows = int.from_bytes(training_images.read(4), byteorder="big")
         num_cols = int.from_bytes(training_images.read(4), byteorder="big")
 
         image_pixel_count = num_rows * num_cols
 
-        image_data = np.zeros((num_rows, num_cols))
+        image_data = np.zeros((num_images, image_pixel_count))
+        image_label = np.zeros((num_images, 10))
 
-        for row in range(num_rows):
-            for col in range(num_cols):
-                image_data[row][col] = int.from_bytes(training_images.read(1), byteorder="big")
+        for image in range(2):
+            label = int.from_bytes(training_labels.read(1), byteorder="big")
+            image_label[image][label] = 1.0
+            for pixel in range(image_pixel_count):
+                image_data[image][pixel] = int.from_bytes(training_images.read(1), byteorder="big")
 
     finally:
         training_images.close()
-        print(num__images, num_cols, num_rows, image_pixel_count)
-        print(image_data)
+        print(display(image_data[1]), image_label[1])
         print("Success")
+
+
+def display(number):
+    render = ''
+
+    for i in range(len(number)):
+        if i % 28 == 0:
+            render = render + '\n'
+        if number[i] > 200:
+            render = render + '#'
+        else:
+            render = render + '.'
+
+    return render
+
 
 load_data()
