4952accad5b0e8a1cd2348ade1e58fd7ab7a8fe5
Soraphol (Paul) Damrongpiriyapong <pauldamrong@gmail.com>
2018-05-03 11:25:58 -0400

    Added network object

--- neural_network.py
+++ neural_network.py
@@ -8,6 +8,13 @@ class Network(object):
         self.biases = [np.random.randn(neurons, 1) for neurons in layers[1:3]]
         self.weights = [np.random.rand(layer1_2, layer2_3) for layer1_2, layer2_3 in zip(layers[0:2], layers[1:3])]
 
+    
+
 
 def sigmoid(x):
     return 1.0 / (1.0 + np.exp(-x))
+
+
+def sigmoid_prime(x):
+    return np.exp(x)/np.power(np.exp(x)+1, 2)
+
